acb53c726da416b25f2ec6099bfc2faeadcab440
beezay bohora <bijay.okur@gmail.com>
2020-10-06 15:29:28 +0545

    JWT token Created

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://admin:admin123@cluster0.wchz6.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://admin:admin123@cluster0.wchz6.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 }
