839297932edfba569b47addc2bbdedd56c0c3d9a
beezay bohora <bijay.okur@gmail.com>
2020-10-07 12:15:53 +0545

    Add profile route created

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -32,4 +32,88 @@ router.get('/', passport.authenticate('jwt', {session:false}), (req, res) => {
         .catch(err => res.status(404).json(err))
 })
 
+// @route   POST api/profiles
+// @desc    Create/Update User Profile
+// @access  Private
+router.post('/', passport.authenticate('jwt', {session:false}), (req, res) => {
+    //GET FIELDS
+    const profileFileds = {};
+
+    profileFileds.user = req.user.id;
+    if(req.body.handle) {
+        profileFileds.handle = req.body.handle;
+    }
+    if(req.body.company) {
+        profileFileds.company = req.body.company;
+    }
+    if(req.body.website) {
+        profileFileds.website = req.body.website;
+    }
+    if(req.body.location) {
+        profileFileds.location = req.body.location;
+    }
+    if(req.body.bio) {
+        profileFileds.bio = req.body.bio;
+    }
+    if(req.body.status) {
+        profileFileds.status = req.body.status;
+    }
+    if(req.body.github) {
+        profileFileds.github = req.body.github;
+    }
+    //SKILLS
+    if(typeof req.body.skills !== 'undefined') {
+        profileFileds.skills = req.body.skills.split(',');
+    }
+    //SOCIAL
+    profileFileds.social = {};
+    if(req.body.youtube) {
+        profileFileds.social.youtube = req.body.youtube
+    }
+    if(req.body.twitter) {
+        profileFileds.social.twitter = req.body.twitter
+    }
+    if(req.body.facebook) {
+        profileFileds.social.facebook = req.body.facebook
+    }
+    if(req.body.linkedin) {
+        profileFileds.social.linkedin = req.body.linkedin
+    }
+    if(req.body.instagram) {
+        profileFileds.social.instagram = req.body.instagram
+    }
+
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+                if(profile) {
+                    //Update
+                    Profile.findOneAndUpdate(
+                        {user: req.user.id},
+                        {$set: profileFileds},
+                        {new: true}
+                    )
+                    .then(profile => res.json(profile))
+                }
+                else{
+                    //CREATE NEW
+
+                    //Check if Handle Exist
+                    Profile.findOne({ handle: profileFileds.handle })
+                        .then(profile => {
+                            if(profile) {
+                                errors.handle = 'That Handle already Exist';
+                                res.status(400).json(errors);
+                            }
+                        })
+
+                    // Save Profile
+                    new Profile(profileFileds)
+                        .save()
+                        .then(profile => res.json(profile))
+                }
+        })
+    
+})
+
+
 module.exports = router;
